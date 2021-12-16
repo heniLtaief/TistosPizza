97837f1d2ccd27446d58b4ad6d0d80b1f024c1fd
heniLtaief <heniltaief@gmail.com>
2021-12-16 15:38:25 +0100

    working on toggle sidebar

--- titos/src/Hero/index.js
+++ titos/src/Hero/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import {HeroContainer, HeroContent,HeroBtn,HeroH1,HeroP,HeroItems} from "./HeroElements"
+import {HeroContainer, HeroContent,HeroBtn,HeroH1,HeroP,HeroItems} from "./HeroElements";
+
 
 const Hero = () => {
+    const [isOpen,setIsOpen]= useState(false)
+
+    const toggle=()=>{
+        setIsOpen=(!isOpen)
+    }
     return (
         <HeroContainer>
-           <Navbar/>
-           <Sidebar/>
+           <Navbar toggle={toggle}/>
+           <Sidebar toggle={toggle} isOpen={isOpen}/>
            <HeroContent>
                <HeroItems>
                    <HeroH1>The Greatest Pizza Ever</HeroH1>
--- titos/src/components/Sidebar/SidebarElements.js
+++ titos/src/components/Sidebar/SidebarElements.js
@@ -44,3 +44,41 @@ export const SidebarMenu = styled.div`
   }
 `;
 
+export const SidebarLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  color: #000;
+  cursor: pointer;
+  &:hover {
+    color: #e31837;
+    transition: 0.2s ease-in-out;
+  }
+`;
+
+export const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const SidebarRoute = styled(Link)`
+  background: #e31837;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #fff;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    transition: 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
+`;
