33caa372209c9027b54dd0a228ccef969802a418
heniLtaief <heniltaief@gmail.com>
2021-12-16 15:18:11 +0100

    start sidebar

---
+++ titos/src/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+            <CloseIcon/>
+            </Icon>
+            <sidebarMenu>
+                
+            </sidebarMenu>               
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
