69350904d33a958028f034a3735409a05ec4cafb
heniLtaief <heniltaief@gmail.com>
2021-12-16 14:05:52 +0100

    creating globalstyle

--- titos/src/components/Navbar/NavbarElements.js
+++ titos/src/components/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {NavLink as Link} from "react-router-dom"
+import { FaPizzaSlice } from 'react-icons/fa';
 
 export const Nav= styled.nav`
 background: red;
