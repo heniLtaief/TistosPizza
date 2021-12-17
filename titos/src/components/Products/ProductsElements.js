17f0fd7cbd2f5694559a5ef67a7091a8ebb5beb0
heniLtaief <heniltaief@gmail.com>
2021-12-17 10:59:25 +0100

    head & title & infos

--- titos/src/components/Products/ProductsElements.js
+++ titos/src/components/Products/ProductsElements.js
@@ -27,3 +27,24 @@ export const ProductImg = styled.img`
   max-width: 100%;
   box-shadow: 8px 8px #fdc500;
 `;
+
+export const ProductsHeading = styled.h1`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+`;
+
+export const ProductTitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.5rem;
+`;
+
+export const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  text-align: center;
+`;
+
