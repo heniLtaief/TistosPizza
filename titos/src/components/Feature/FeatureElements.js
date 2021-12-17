6c8a57884efd139350a88f81c82d55fa4a3f8ca0
heniLtaief <heniltaief@gmail.com>
2021-12-17 11:13:51 +0100

    feature button

--- titos/src/components/Feature/FeatureElements.js
+++ titos/src/components/Feature/FeatureElements.js
@@ -22,3 +22,18 @@ h1 {
     margin-bottom: 1rem;
     font-size: clamp(1rem, 3vw, 2rem);
   }`;
+
+  export const FeatureButton = styled.button`
+  font-size: 1.4rem;
+  padding: 0.6rem 3rem;
+  border: none;
+  background: #ffc500;
+  color: #000;
+  transition: 0.2s ease-out;
+  &:hover {
+    color: #fff;
+    background: #e31837;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+}
+`;
